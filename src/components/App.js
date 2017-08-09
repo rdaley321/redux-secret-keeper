f191de502b7033ce8247e291d037c5fe11eeefeb
Ryan Daley <rdaley321@yahoo.com>
2017-08-08 20:44:57 -0500

    homepage styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <h1>Got Secrets?</h1>
+        <p>Your Trapper-Keeper&trade; for secrets. Sign up today. It's easy, free, and about as reliable as somebody you sort of trust!</p>
+        <hr></hr>
+        <p className="w3-opacity">Create an account to get started, or log back in if you're already a Secret Keeper.</p>
+        <Link to="/signup"><button className="w3-btn w3-ripple w3-blue homepage-button" type="button">Sign Up</button></Link>
+        <Link to="/login"><button className="w3-btn w3-ripple w3-green homepage-button" type="button">Log In</button></Link>
       </div>
     );
   }
